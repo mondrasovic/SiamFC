cf6d5628f85140da0ae7212ccf66a692a218195d
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-02-13 07:21:52 +0100

    Change experiment from GOT10k to OTB2013.

--- src/sot/eval.py
+++ src/sot/eval.py
@@ -2,25 +2,26 @@ from typing import Optional
 
 import click
 import torch
-from got10k.experiments import ExperimentGOT10k
+from got10k.experiments import ExperimentOTB
 
-from sot.cfg import DATASET_DIR, TrackerConfig
+from sot.cfg import TrackerConfig
 from sot.tracker import TrackerSiamFC
 
 
 @click.command()
+@click.argument("dataset_dir_path")
 @click.argument("results_dir_path")
 @click.argument("reports_dir_path")
 @click.option("--model-file-path", help="model file path")
 def main(
-        results_dir_path: str, reports_dir_path: str,
+        dataset_dir_path: str, results_dir_path: str, reports_dir_path: str,
         model_file_path: Optional[str]) -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     cfg = TrackerConfig()
     
     tracker = TrackerSiamFC(cfg, device, model_file_path)
-    experiment = ExperimentGOT10k(
-        DATASET_DIR, subset='val', result_dir=results_dir_path,
+    experiment = ExperimentOTB(
+        dataset_dir_path, version=2013, result_dir=results_dir_path,
         report_dir=reports_dir_path)
     
     experiment.run(tracker, visualize=False)
