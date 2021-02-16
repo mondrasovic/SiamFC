Fix cross-correlation computation.

src/sot/model.py
@@ -40,8 +40,6 @@ class SiamFCModel(nn.Module):
     def __init__(self, response_map_scale: float = 0.001) -> None:
         super().__init__()
         
-        assert response_map_scale > 0, "response map scale must be positive"
-        
         self.response_map_scale: float = response_map_scale
         
         self.conv1 = _ConvBlock(
@@ -83,14 +81,13 @@ class SiamFCModel(nn.Module):
     def cross_corr(
             exemplar_emb: torch.Tensor,
             instance_emb: torch.Tensor) -> torch.Tensor:
-        assert exemplar_emb.ndim == instance_emb.ndim
-        assert exemplar_emb.ndim == 4
-        
+        n_exemplars = exemplar_emb.shape[0]
         n_instances, instance_c, instance_h, instance_w = instance_emb.shape
         
-        instance_emb = instance_emb.reshape(1, -1, instance_h, instance_w)
+        instance_emb = instance_emb.reshape(
+            -1, n_exemplars * instance_c, instance_h, instance_w)
         response_map = F.conv2d(
-            input=instance_emb, weight=exemplar_emb, groups=n_instances)
+            input=instance_emb, weight=exemplar_emb, groups=n_exemplars)
         response_map = response_map.reshape(
             n_instances, -1, response_map.shape[-2], response_map.shape[-1])
         

src/tests/test_model.py
@@ -54,6 +54,13 @@ class TestSiamFCModel(unittest.TestCase):
         response_map = self.model.cross_corr(exemplar_emb, instance_emb)
         
         self.assertEqual(list(response_map.shape), [1, 1, 17, 17])
+
+    def test_response_map_shape_one_exemplar_multiple_instances(self):
+        exemplar_emb = torch.ones(1, 10, 6, 6)
+        instance_emb = torch.ones(10, 10, 22, 22)
+        response_map = self.model.cross_corr(exemplar_emb, instance_emb)
+    
+        self.assertEqual(list(response_map.shape), [10, 1, 17, 17])
     
     def test_response_map_shape_multiple_channels_multiple_instances(self):
         exemplar_emb = torch.ones(10, 10, 6, 6)
@@ -61,12 +68,6 @@ class TestSiamFCModel(unittest.TestCase):
         response_map = self.model.cross_corr(exemplar_emb, instance_emb)
         
         self.assertEqual(list(response_map.shape), [10, 1, 17, 17])
-    
-    def test_cross_correlation_invalid_shapes(self):
-        with self.assertRaises(AssertionError):
-            exemplar_emb = torch.ones((1, 1, 1))
-            instance_emb = torch.ones(1, 10, 5, 5)
-            self.model.cross_corr(exemplar_emb, instance_emb)
     
     def test_cross_correlation_white_rectangle_search(self):
         def to_tensor(arr):